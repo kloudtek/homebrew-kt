ed32f3e6658f732b842ce7b59635e744835933db
ymenager <ymenager@users.noreply.github.com>
2019-01-02 10:17:46 -0800

    Update anypoint-tools.rb

--- anypoint-tools.rb
+++ anypoint-tools.rb
@@ -2,7 +2,7 @@ class AnypointTools < Formula
   desc "Library and command-line to interact with mulesoft anypoint"
   homepage "https://github.com/Kloudtek/anypoint-tools"
   url "https://github.com/kloudtek/anypoint-tools/archive/0.9.38.tar.gz"
-  sha256 "86e93fde814ec90b50ab59e85aad85d0733e4ba0837b597ec15bd06ebeec04d0"
+  sha256 "6f32768f4b38acdc0295a1630df6ed07013713854c2051ab25d4ed31274ed7b7"
   depends_on :java => "1.8+"
   def install
     system "sh", "mvnw", "package"
